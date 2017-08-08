48815ffef6846965deed99a4d46266baaf9c6b1b
Henribou <riton@ignition-program.com>
2017-08-08 12:18:41 +0200

    Initialize firebase back

--- cocoaching-front/my-app/src/index.js
+++ cocoaching-front/my-app/src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase'
+import { connect } from 'react-firebase'
+
+
+// Initialize Firebase
+// we use const because it is a read only reference
+const config = {
+    apiKey: "AIzaSyDMUG6hXD-ixEMrSpWm6qG1fJLmyge2f4c",
+    authDomain: "cocoach-app.firebaseapp.com",
+    databaseURL: "https://cocoach-app.firebaseio.com",
+    projectId: "cocoach-app",
+    storageBucket: "cocoach-app.appspot.com",
+    messagingSenderId: "498651165878"
+};
+const firebaseApp = firebase.initializeApp(config);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
